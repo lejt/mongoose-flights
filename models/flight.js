9fbebb1e8dbda6338ac6f4f45fe42276ae4de188
lejt <92820402+lejt@users.noreply.github.com>
2021-12-08 17:03:03 -0800

    use partials for HTML nav format

--- models/flight.js
+++ models/flight.js
@@ -1,3 +1,15 @@
-//define SCHEMA HERE
+//define DB Schema here
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: String,
+    airport: String,
+    flightNo: Number,
+    departs: Date,
+  }, {
+    timestamps: true
+});
+  
+module.exports = mongoose.model('Flight', flightSchema);
