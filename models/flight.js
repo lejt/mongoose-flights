09636aefc72d8dddea4a386aeb39c6152ef65bd2
lejt <92820402+lejt@users.noreply.github.com>
2021-12-09 07:50:06 -0800

    add trim and required for schema

--- models/flight.js
+++ models/flight.js
@@ -7,21 +7,27 @@ const flightSchema = new Schema({
     airline: {
         type: String,
         enum: ["American", "Southwest", "United"],
+        trim: true,
         required: true,
     },
     airport: {
         type: String,
         enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+        trim: true,
+        required: true,
     },
     flightNo: {
         type: Number,
         min: 10,
         max: 9999,
+        trim: true,
+        required: true,
     },
     departs: {
         type: Date,
         default: new Date(+new Date() + 365*24*60*60*1000),
         min: () => new Date(),
+        trim: true,
     },
 });
   
