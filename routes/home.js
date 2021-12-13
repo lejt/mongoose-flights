11cb40ec252a0c1a3ba111563cddf8ce4614939c
lejt <92820402+lejt@users.noreply.github.com>
2021-12-12 21:26:32 -0800

    start add css

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Get Home Page
 router.get('/', function(req, res, next) {
-  res.render('index', );
+  res.render('home');
 });
 
 module.exports = router;
